e12fcefbcd4c2d2b0178b5de369116cb6973988b
Sudhanshu Patel <144509389+SudhanshuPatel1707@users.noreply.github.com>
2024-07-24 02:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import helper
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy
 import plotly.figure_factory as ff
 
 df = pd.read_csv('athlete_events.csv')
